e4d28bbd02a822039aac7d1615031e22d7fbf078
Michael <mickeyfowose@gmail.com>
2023-08-07 20:36:47 +0100

    update

--- lib/data/routine_database.dart
+++ lib/data/routine_database.dart
@@ -2,6 +2,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 import 'package:flutter/material.dart';
 
+//reference our box
 final _myBox = Hive.box('Routine_Database');
 
 class RoutineDatabase{
@@ -23,7 +24,7 @@ void updateDatabase(){
 _myBox.put('CURRENT_ROUTINE_LIST', routineList);
 }
 
-
+//load data if it already exist
 void loadData(){
   routineList = _myBox.get('CURRENT_ROUTINE_LIST');
 
